72cc4b3a7268f352ac52bf4261f7460654eb6385
v2up-32mb <v2up-32mb@users.noreply.github.com>
2026-09-06 10:12:51 +0800

    slim: drop cifs/nfs/samba/lucky/openclash/openlist2/zerotier, keep sqm+ebpf base for manual daed

--- scripts/03_prepare_package.sh
+++ scripts/03_prepare_package.sh
@@ -11,16 +11,6 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 # samba4 default config
 sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
 
-# clash_meta
-mkdir -p files/etc/openclash/core
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
-wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
-chmod +x files/etc/openclash/core/clash*
-
-# clash_config
-mkdir -p files/etc/config
-wget -qO- https://raw.githubusercontent.com/Kwonelee/Kwonelee/refs/heads/main/rule/openclash > files/etc/config/openclash
-
 # 集成无线驱动
 mkdir -p package/base-files/files/lib/firmware/brcm
 cp -a $GITHUB_WORKSPACE/configfiles/firmware/brcm/* package/base-files/files/lib/firmware/brcm/
@@ -59,11 +49,6 @@ function git_sparse_clone() {
 }
 
 # 常见插件
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-git_sparse_clone main https://github.com/gdy666/luci-app-lucky luci-app-lucky lucky
-cp -f $GITHUB_WORKSPACE/configfiles/lucky_status.htm package/new/luci-app-lucky/luasrc/view/lucky/lucky_status.htm
-git_sparse_clone main https://github.com/sbwml/luci-app-openlist2 luci-app-openlist2 openlist2
-git_sparse_clone main https://github.com/sbwml/openwrt_pkgs luci-app-zerotier
 git_sparse_clone main https://github.com/Kwonelee/openwrt-packages luci-app-ramfree filebrowser luci-app-filebrowser-go
 FB_VERSION="$(curl -s https://github.com/filebrowser/filebrowser/tags | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' | head -n 1 | sed 's/^v//')"
 sed -i "s/2.54.0/$FB_VERSION/g" package/new/filebrowser/Makefile
